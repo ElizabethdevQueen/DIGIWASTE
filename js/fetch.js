819390f5cecf7a33dd535ce9466817472bdf4761
Elizabeth Segun-Ajiboye <elizabethaderemi@gmail.com>
2020-12-11 18:24:58 +0100

    updated home page

--- js/fetch.js
+++ js/fetch.js
@@ -1,56 +1,9 @@
-// ==========fetching register/signup api from backend
-
-function onRegister () {
-    const querySelectorData = [
-        'landlord_name',
-        'compound_name',
-        'compound_number',
-        'email',
-        'phone_number',
-        'password',
-        'address',
-        'exampleFormControlSelect1',
-    ];
-
-    const querySelectorElements = querySelectorData.map((attrVal) => {
-        const { name, value } = document.getElementById(attrVal);
-        return { name, value };
-    });
-
-    const headers = new Headers();
-    headers.append("Content-Type", "application/json");
-
-    const body = {}
-
-    querySelectorElements.forEach(({ name, value }) => {
-        if (name === 'packages') {
-            body[name] = [value.toLowerCase()];
-            return null;
-        }
-        body[name] = value;
-    });
-
-    fetch('https://ngrecycle.herokuapp.com/api/v1/auth/register', {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(body),
-        redirect: 'follow',
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        localStorage.setItem('userdetails', JSON.stringify(data));
-        localStorage.getItem('userdetails');
-        location.replace('http://localhost:5501/DIGIWASTE/user.html')
-    })
-    .catch((err) => console.log(err));
-};
-
 
 // ========= login fetch api
 function onLogin() {
     const querySelectorData = [
         'email',
-        'password'
+        'password',
     ]
     const querySelectorElements = querySelectorData.map((attrVal) => {
         const { name, value } = document.getElementById(attrVal);
@@ -79,3 +32,73 @@ function onLogin() {
     })
     .catch((err) => console.log(err));
 }
+
+
+// =============user logout ===============
+function onLogout() {
+    const querySelectorData = [
+        'email',
+        'password'
+    ]
+    const querySelectorElements = querySelectorData.map((attrVal) => {
+        const { name, value } = document.getElementById(attrVal);
+        return { name, value };
+    });
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const body = {}
+
+    querySelectorElements.forEach(({ name, value }) => {
+        body[name] = value;
+    });
+
+    fetch('https://ngrecycle.herokuapp.com/api/v1/auth/logout', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body),
+        redirect: 'follow'
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            localStorage.setItem('userdetails', JSON.stringify(data));
+            localStorage.getItem('userdetails');
+            location.replace('http://localhost:5501/DIGIWASTE/index.html')
+        })
+        .catch((err) => console.log(err));
+}
+
+// =======Admin login=======
+function onAdminLogin() {
+    const querySelectorData = [
+        'email',
+        'password'
+    ]
+    const querySelectorElements = querySelectorData.map((attrVal) => {
+        const { name, value } = document.getElementById(attrVal);
+        return { name, value };
+    });
+    const headers = new Headers();
+    headers.append("Content-Type", "application/json");
+
+    const body = {}
+
+    querySelectorElements.forEach(({ name, value }) => {
+        body[name] = value;
+    });
+
+    fetch('https://ngrecycle.herokuapp.com/api/v1/admin/login', {
+        method: 'POST',
+        headers,
+        body: JSON.stringify(body),
+        redirect: 'follow'
+    })
+        .then((res) => res.json())
+        .then((data) => {
+            localStorage.setItem('userdetails', JSON.stringify(data));
+            localStorage.getItem('userdetails');
+            location.replace('http://localhost:5501/DIGIWASTE/admin.html')
+        })
+        .catch((err) => console.log(err));
+}
+
